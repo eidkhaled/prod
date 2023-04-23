c1a7188cf6086b784865eb0344dd2c3bea357601
eid khaled <104042214+eidkhaled@users.noreply.github.com>
2023-04-23 07:01:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "9092"
+};
+using var Producer=new ProducerBuilder<Null,String>(config).Build();
+try
+{
+    string? State; 
+    while ((State=Console.ReadLine())!=null)
+    {
+        var response = await Producer.ProduceAsync("eid Topic",
+        new Message<Null, string> { Value =JsonConvert.SerializeObject(new Weather( 
+        State,70))});
+
+    }
+    
+         
+
+}
+catch(ProduceException<Null,string> exc)
+{
+    Console.WriteLine(exc.Message);
+}
+public record Weather(string State,int temp);
